b7bc2aff16bffe0678056ae6a5d9a4cb52ca23ca
Thomas <tmrtw8@gmail.com>
2023-02-28 18:32:54 -0700

    added_initial_variables

---
+++ assets/js/index.js
@@ -0,0 +1,72 @@
+// variables:
+var start = document.getElementById("start");
+var quiz_div = document.getElementById("quiz_div");
+var timer = document.getElementById("timer");
+var timer_seconds = document.getElementById("timer_seconds");
+var quiz_question = document.getElementById("quiz_question");
+var quiz_list = document.getElementById("quiz_list");
+var results_div = document.getElementById("results_div");
+var complete_msg = document.getElementById("complete_msg");
+var score = document.getElementById("score");
+var restart = document.getElementById("restart");
+
+
+
+let questions = [
+    {
+    numb: 1,
+    question: " ?",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 2,
+    question: " ?",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 3,
+    question: " ?",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 4,
+    question: " ?",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+    {
+    numb: 5,
+    question: " ?",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+
+];
